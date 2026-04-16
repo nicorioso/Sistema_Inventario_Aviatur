d98330e1f12e78b474b493d33899656082b525e6
nicorioso <nicoriosops4@gmail.com>
2026-04-16 17:08:33 -0500

    primer commit

---
+++ src/main/java/com/aviatur/sgia/backend/SgiaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aviatur.sgia.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgiaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgiaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aviatur/sgia/backend/SgiaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aviatur.sgia.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgiaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
